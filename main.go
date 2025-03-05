7c3c9712b929ed51d5bf37006f2819d4cbd4808b
Maxim Gromovoy <maxku98@gmail.com>
2025-03-05 14:50:22 +0300

    input from console

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	fmt "fmt"
+
 	"./chessBoard"
 )
 
 func main() {
-	CreateAndPrintBoard(10)
+	var size uint
+
+	fmt.Print("Input board size: ")
+	fmt.Scan(&size)
+
+	CreateAndPrintBoard(size)
 }
 
 func CreateAndPrintBoard(boardSize uint) {
