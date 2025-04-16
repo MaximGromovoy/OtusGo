58aae4c8c72a64abfb2e93fd168f8f2525b91ef2
Maxim Gromovoy <maxku98@gmail.com>
2025-04-16 18:51:52 +0300

    Add new logic

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"OtusGo/internal/model/currency"
+	"OtusGo/internal/repository"
+	"OtusGo/internal/service"
+	"fmt"
+)
 
 func main() {
+	var currencies []currency.CurrencyInterface = service.GetRandomCurrencies()
 
-	fmt.Println("Hello Go from feature branch!")
+	rubles, dollars, euros, liras := repository.DistributeCurrencies(currencies)
+
+	fmt.Println("Rubles:")
+	for _, ruble := range rubles {
+		fmt.Printf("Name: %s, Code: %s, Value: %.2f\n", ruble.GetName(), ruble.GetCode(), ruble.GetValue())
+	}
+
+	fmt.Println("\nDollars:")
+	for _, dollar := range dollars {
+		fmt.Printf("Name: %s, Code: %s, Value: %.2f\n", dollar.GetName(), dollar.GetCode(), dollar.GetValue())
+	}
+
+	fmt.Println("\nEuros:")
+	for _, euro := range euros {
+		fmt.Printf("Name: %s, Code: %s, Value: %.2f\n", euro.GetName(), euro.GetCode(), euro.GetValue())
+	}
+
+	fmt.Println("\nLiras:")
+	for _, lira := range liras {
+		fmt.Printf("Name: %s, Code: %s, Value: %.2f\n", lira.GetName(), lira.GetCode(), lira.GetValue())
+	}
 }
